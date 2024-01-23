43a2199282f953ac843847cb6b844790f7c638a5
msavarian <sav68.net@gmail.com>
2024-01-23 20:41:05 +0330

    create TicketDetails

---
+++ src/CustomerUI/Pages/TicketDetails.cshtml.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CustomerUI.Pages
+{
+    public class TicketDetailsModel : PageModel
+    {
+
+
+        #region Model
+        [BindProperty]
+        public int Id { get; set; }
+        #endregion
+
+
+        public void OnGet(int id)
+        {
+            this.Id=id;
+        }
+    }
+}
